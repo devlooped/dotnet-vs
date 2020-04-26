717ddd590132810c700a0d0c9a596edf12ee2060
Adrian Alonso <adalonso@microsoft.com>
2020-04-26 14:14:47 -0300

    Added UpdateCommand skeleton

---
+++ VisualStudio/UpdateCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace VisualStudio
+{
+    class UpdateCommand : Command<UpdateCommandDescriptor>
+    {
+        public UpdateCommand(UpdateCommandDescriptor descriptor) : base(descriptor) { }
+
+        public override async Task ExecuteAsync(TextWriter output)
+        {
+            var instances = await WhereService.Instance.GetAllInstancesAsync(Descriptor.Sku, Descriptor.Channel);
+
+            foreach (var instance in instances)
+                output.WriteLine("Updating " + instance.InstallationPath + "...");
+        }
+    }
+}
