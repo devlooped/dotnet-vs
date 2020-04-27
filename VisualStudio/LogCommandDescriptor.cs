80009dbf13ce31c80ad8b3d0476b4f407be8fe2c
Daniel Cazzulino <daniel@cazzulino.com>
2020-04-27 17:17:44 -0300

    Add `vs log` command to open the ActivityLog.xml

---
+++ VisualStudio/LogCommandDescriptor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Mono.Options;
+
+namespace VisualStudio
+{
+    class LogCommandDescriptor : CommandDescriptor
+    {
+        readonly VisualStudioOptions options = new VisualStudioOptions(channelVerb: "Open", showNickname: false);
+        bool exp;
+
+        public LogCommandDescriptor() => OptionSet = new CompositeOptionSet(options, new OptionSet
+            {
+                { "exp", "Use experimental instance instead of regular.", e => exp = e != null },
+            });
+
+        public Channel? Channel => options.Channel;
+
+        public Sku? Sku => options.Sku;
+
+        public bool Experimental => exp;
+    }
+}
