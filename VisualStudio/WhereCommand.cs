51b96f9c54d81c76b552dc3192593f87e3acb2ff
Adrian Alonso <adalonso@microsoft.com>
2020-04-28 20:52:50 -0300

    Improved WhereCommand
    To be complaint and consistent with the rest of commands by using the chooser and Channel/Sku arguments.

--- VisualStudio/WhereCommand.cs
+++ VisualStudio/WhereCommand.cs
@@ -18,10 +18,36 @@ namespace VisualStudio
 
         public IEnumerable<VisualStudioInstance> Instances { get; private set; } = Enumerable.Empty<VisualStudioInstance>();
 
-        public override Task ExecuteAsync(TextWriter output) =>
-            whereService.RunAsync(
-                   Descriptor.Sku,
-                   Descriptor.WorkloadsArguments.Concat(Descriptor.ExtraArguments),
-                   output);
+        public override async Task ExecuteAsync(TextWriter output)
+        {
+            var instances = await whereService.GetAllInstancesAsync(
+                Descriptor.Sku,
+                Descriptor.Channel,
+                Descriptor.WorkloadsArguments.Concat(Descriptor.ExtraArguments));
+
+            if (!Descriptor.ShowAll)
+                instances = new Chooser("show").ChooseMany(instances, output);
+
+            foreach (var instance in instances)
+                ShowProperties(instance, output);
+        }
+
+        void ShowProperties(VisualStudioInstance instance, TextWriter output)
+        {
+            output.WriteLine();
+            output.WriteLine($"{ instance.DisplayName} - Version { instance.Catalog.ProductDisplayVersion}");
+
+            var props = GetProperties(instance, "Catalog", "Properties").ToList();
+            props.AddRange(GetProperties(instance.Catalog));
+            props.AddRange(GetProperties(instance.Properties));
+
+            foreach (var prop in props)
+                output.WriteLine($"{prop.PropertyName}: {prop.PropertyValue}");
+        }
+
+        IEnumerable<(string PropertyName, object PropertyValue)> GetProperties<T>(T instance, params string[] skipProps) =>
+            instance.GetType().GetProperties()
+                .Where(x => skipProps == null || !skipProps.Contains(x.Name))
+                .Select(x => (x.Name, x.GetValue(instance)));
     }
 }
