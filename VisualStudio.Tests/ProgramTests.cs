ccac5e6d469231d92489d71dd3d90dcda7aeef76
Adrian Alonso <adalonso@microsoft.com>
2020-05-02 18:18:39 -0300

    Added save option usage test

--- VisualStudio.Tests/ProgramTests.cs
+++ VisualStudio.Tests/ProgramTests.cs
@@ -9,7 +9,7 @@ namespace VisualStudio.Tests
     {
         readonly TextWriter output;
 
-        public ProgramTests(ITestOutputHelper output) => 
+        public ProgramTests(ITestOutputHelper output) =>
             this.output = new OutputHelperTextWriter(output);
 
         [Fact]
@@ -22,6 +22,18 @@ namespace VisualStudio.Tests
             Assert.Equal(0, exit);
 
             Assert.IsType<RunCommand>(program.Command);
+        }
+
+        [Fact]
+        public async Task when_save_option_is_specified_then_save_command_is_executed()
+        {
+            var program = new Program(output, false, "run", "Enteprise", "--save=foo");
+
+            var exit = await program.RunAsync();
+
+            Assert.Equal(0, exit);
+
+            Assert.IsType<SaveCommand>(program.Command);
         }
     }
 }
