341affab2d5bae12b9183ea7312d06d65be4f6c4
Adrian Alonso <adalonso@microsoft.com>
2020-05-05 20:38:16 -0300

    Showing the --release option

--- VisualStudio/Options/ChannelOption.cs
+++ VisualStudio/Options/ChannelOption.cs
@@ -11,7 +11,7 @@ namespace VisualStudio
         public ChannelOption(string verb, Channel? defaultValue = default) : base(defaultValue)
         {
             // This is the default, that's why it's hidden
-            Add("rel|release", verb + " release version", _ => Value = Channel.Release, hidden: true);
+            Add("rel|release", verb + " release version", _ => Value = Channel.Release);
             Add("pre|preview", verb + " preview version", _ => Value = Channel.Preview);
             Add("int|internal", verb + " internal (aka 'dogfood') version", _ => Value = Channel.IntPreview);
             Add("master", verb + " master version", _ => Value = Channel.Master, hidden: true);
